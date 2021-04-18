4774012a6df9b698569c55aeea467fec374068cf
Niharika Gurnani <niharikagurnani13@gmail.com>
2021-04-18 12:57:29 +0530

    fetch posts via api & display

--- src/components/PostsList.vue
+++ src/components/PostsList.vue
@@ -1,12 +1,30 @@
 <template>
   <div class="posts-list">
     <h1>Codeial-Posts</h1>
+    <ul>
+        <li v-for="post in posts" :key="post._id" class="post-item">
+            {{post.content}} --> {{post.user.name}}            
+        </li>
+    </ul>
   </div>
 </template>
 
 <script>
+// data() : returns data associated with the component. 
+// mounted() : initially executes when the component is loaded for the first time.
+// using axios to fetch data from the api.
+import axios from 'axios';
 export default {
-  name: "PostsList"
+  name: "PostsList",
+  data(){
+      return{
+          posts : []
+      }
+  },
+  mounted(){
+      axios.get('http://localhost:8000/api/v1/posts').
+      then((data) => {this.posts = data.data.posts});
+  }
 };
 </script>
 
